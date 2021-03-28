12de471452f10901de4f606367e4869f13af474f
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-03-29 01:51:14 +0600

    firebase config update

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyChkbg-SxxZ1mQQuD0Zwa7wp6lwzaf9Y4A",
+  authDomain: "burj-al-arab-firebase-mern.firebaseapp.com",
+  projectId: "burj-al-arab-firebase-mern",
+  storageBucket: "burj-al-arab-firebase-mern.appspot.com",
+  messagingSenderId: "125883059556",
+  appId: "1:125883059556:web:e5623cadcb77606b1dfe71"
+};
+export default firebaseConfig;
